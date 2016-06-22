7eccb1e8d84e85d53e321b441f1f20937cbe2da9
Niem Nguyen <luuniem87@gmail.com>
2016-06-22 08:39:32 -0700

    minor styling of index page

--- app/models/review.js
+++ app/models/review.js
@@ -1,8 +1,8 @@
-import Model from 'ember-data/model';
+import DS from 'ember-data';
 // import attr from 'ember-data/attr';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
-export default Model.extend({
+export default DS.Model.extend({
 	username: DS.attr(),
 	rating: DS.attr(),
 	comment: DS.attr(),
